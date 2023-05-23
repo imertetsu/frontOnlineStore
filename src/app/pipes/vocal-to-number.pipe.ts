f856a160c1a234d2a0ab4690cb3d31a652329683
unknown <imertpro@gmail.com>
2023-05-23 17:42:00 -0400

    pipes to change vowels to numbers

--- src/app/pipes/vocal-to-number.pipe.ts
+++ src/app/pipes/vocal-to-number.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class VocalToNumberPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    return value.replace(/a/gi, '4')
+                .replace(/e/gi, '3')
+                .replace(/i/gi, '1')
+                .replace(/o/gi, '0')
+                .replace(/u/gi, '12');
   }
-
 }
